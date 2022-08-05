3adfa041f1408c935c182add1fe7dc1e995c79cf
yossu-y <yangyuub@gmail.com>
2022-08-05 11:00:27 +0900

    update

--- reversi/script.js
+++ reversi/script.js
@@ -2,6 +2,45 @@ const stage = document.getElementById("stage");
 const squareTemplate = document.getElementById("square-template");
 const stoneStateList = [];
 
+let currentColor = 1;
+const getReversibleStones = (idx) => {
+  const squareNums = [
+    7 - (idx % 8),
+    Math.min(7 - (idx % 8), (56 + (idx % 8) - idx) / 8),
+    (56 + (idx % 8) - idx) / 8,
+    Math.min(idx % 8, (56 + (idx % 8) - idx) / 8),
+    idx % 8,
+    Math.min(idx % 8, (idx - (idx % 8)) / 8),
+  ];
+
+  const parameters = [1, 9, 8, 7, -1, -9, -8, -7];
+
+  let results = [];
+
+  for (let i = 0; i < 8; i++) {
+    const box = [];
+    const squareNum = squareNums[i];
+    const params = parameters[i];
+    const nextStoneState = stoneStateList[idx + params];
+
+    if (nextStoneState === 0 || nextStoneState === currentColor) continue;
+    box.push(idx + params);
+
+    for (let j = 0; j < squareNum - 1; j++) {
+      const targetIdx = idx + params * 2 + params * j;
+      const targetColoor = stoneStateList[targetIdx];
+      if (targetColor === 0) continue;
+      if (targetColor === currentColor) {
+        results = results.concat(box);
+        break;
+      } else {
+        box.push(targetIdx)
+      }
+    }
+  }
+  return results;
+};
+
 const onClickSquare = (index) => {
   // console.log(index);
   if (stoneStateList [index] !== 0) {

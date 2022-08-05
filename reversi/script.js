95f8f57068d26d0e41eb657fda23f3f50d3cb936
yossu-y <yangyuub@gmail.com>
2022-08-05 09:19:17 +0900

    update

---
+++ reversi/script.js
@@ -0,0 +1,29 @@
+const stage = document.getElementById("stage");
+const squareTemplate = document.getElementById("square-template");
+
+const createSquares = () => {
+  for (let i = 0; i < 64; i++) {
+    const square = squareTemplate.cloneNode(true);
+    square.removeAttribute("id");
+    stage.appendChild(square);
+
+    const stone = square.querySelector(".stone");
+
+    let defaultState;
+    if (i == 27 || i == 36) {
+      defaultState = 1;
+    } else if (i == 28 || i == 35) {
+      defaultState = 2;
+    } else {
+      defaultState = 0;
+    }
+
+    stone.setAttribute("data-state", defaultState);
+  }
+};
+
+
+
+window.onload = () => {
+  createSquares();
+};

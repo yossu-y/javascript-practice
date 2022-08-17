2332b6881e8066f4f9e7dc1ffa97b5e7791ffd15
yossu-y <yangyuub@gmail.com>
2022-08-17 19:18:42 +0900

    update

--- udemy/front-dev-tutorial-master/04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ udemy/front-dev-tutorial-master/04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -2,7 +2,18 @@ const str = 'animation';
 const strArry = str.split('');
 
 function tag(accu, curr) {
+    console.log(accu, curr);
     return `${accu}<${curr}>`;
+}
+
+function reduce(arry, callback, defaultValue) {
+    let accu = defaultValue;
+    for(let i = 0; i < arry.length; i++) {
+        let curr = arry[i];
+        accu = callback(accu, curr);
+    }
+
+    return accu;
 }
 
 const result = strArry.reduce(tag, "");
